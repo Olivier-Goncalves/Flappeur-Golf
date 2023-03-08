cae2fd38aff10d5152a1e6b0804b6e60006da685
Olivier-Goncalves <202155887@edu.clg.qc.ca>
2023-03-07 22:00:50 -0500

    Ajustement Collision Caméra

--- Assets/Scripts/MouseControl.cs
+++ Assets/Scripts/MouseControl.cs
@@ -1,22 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.UI;
+// source: https://www.youtube.com/watch?v=vpn8CbPpvlU
 public class MouseControl : MonoBehaviour
 {
-    [SerializeField] private float speed = 1;
-
+    [SerializeField] private float sensitivité = 1;
+    [SerializeField] private Transform camera;
+    private float camDistance;
+    private Vector2 distanceCameraMinMax = new Vector2(0.4f, 7f);
+    private Vector3 directionCamera;
     private float x;
     private float y;
-
+    private void Start()
+    {
+        directionCamera = camera.transform.localPosition.normalized;
+        camDistance = distanceCameraMinMax.y;
+    }
+    
     void Update()
     {
+        
         Cursor.lockState = CursorLockMode.Confined;
-        x += speed * Input.GetAxis("Mouse X");
-        y -= speed * Input.GetAxis("Mouse Y");
-
-        transform.eulerAngles = new Vector3(0, x, 0f);
+        x += sensitivité * Input.GetAxis("Mouse X");
+        y -= sensitivité * Input.GetAxis("Mouse Y");
+        transform.eulerAngles =  new Vector3(y, x, 0f);
+        AjusterPositionCamera();
     }
+
+    public void AjusterPositionCamera()
+    {
+        Vector3 positionCameraVoulu = transform.TransformPoint(directionCamera * distanceCameraMinMax.y);
+        RaycastHit hit;
+        if (Physics.Linecast(transform.position, positionCameraVoulu, out hit))
+        {
+            camDistance = Mathf.Clamp(hit.distance - 0.1f, distanceCameraMinMax.x + 0.2f, distanceCameraMinMax.y);
+        }
+        else
+        {
+            camDistance = distanceCameraMinMax.y + 0.2f;
+        }
+
+        camera.localPosition = directionCamera * camDistance;
+    }
+
+    
 }
