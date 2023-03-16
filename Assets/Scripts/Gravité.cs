ede6553c6f38b29afc55ccd6780997fa6cfff908
GuiFlam <gui.flamand@gmail.com>
2023-03-16 15:02:54 -0400

    Zones gravitées complétées et prefabs

--- Assets/Scripts/Gravité.cs
+++ Assets/Scripts/Gravité.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Gravité : MonoBehaviour
 {
+    private int layeraccelere = 11;
+    private int layerinverse = 12;
+    private int layeraccelereinverse = 13;
     private Vector3 gravity;
     private Rigidbody _rigidbody;
     private void Awake()
@@ -18,11 +21,22 @@ public class Gravité : MonoBehaviour
         _rigidbody.AddForce(gravity, ForceMode.Acceleration);
     }
     
-    void ReverseGravity() => gravity *= -6f;
 
     private void OnTriggerEnter(Collider other)
     {
-        ReverseGravity();
+        if (other.gameObject.layer == layeraccelere)
+        {
+            gravity *= 10f;
+        }
+        if (other.gameObject.layer == layerinverse)
+        {
+            gravity *= -6f;
+        }
+        if (other.gameObject.layer == layeraccelereinverse)
+        {
+            gravity *= -12;
+        }
+        
     }
 
     private void OnTriggerExit(Collider other)
